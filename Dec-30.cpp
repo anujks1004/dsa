36f72d3caf0378bb94d871b612131f417f771fb0
anujks1004 <anuj.singh100420@gmail.com>
2023-12-30 17:17:36 +0530

    Leetcode 1897 Redistribute Characters to Make All Strings Equal

---
+++ Dec-30.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        int n = words.size();
+        vector<int>freq(26,0);
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < words[i].length(); j++)
+                freq[words[i][j]-'a']++;
+        }
+
+        for(int i = 0; i < 26; i++)
+            if(freq[i] % n)
+                return 0;
+        
+        return 1;
+
+    }
+};
