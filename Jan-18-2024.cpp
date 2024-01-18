ea0dfb6c8f7ee04d62a49dcc74d3c3e273d69e93
anujks1004 <anuj.singh100420@gmail.com>
2024-01-18 22:43:07 +0530

    Leetcode 70. Climbing Stairs

---
+++ Jan-18-2024.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev2 = 1;
+        int prev1 = 1;
+        for(int i = 2; i < n + 1; i++)
+        {
+            int curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr;
+        }    
+        return prev1;
+    }
+};
